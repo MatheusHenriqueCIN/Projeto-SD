0675993d09798238741b860a8dff7c09b2d10c7a
Matheus Stepple <156907450+MatheusStepple@users.noreply.github.com>
2025-04-14 23:48:30 -0300

    Update resultado_teste.sv

--- modules/Data Memory/resultado_teste.sv
+++ modules/Data Memory/resultado_teste.sv
@@ -1,6 +1,9 @@
-
-[2025-04-15 01:53:17 UTC] iverilog '-Wall' '-g2012' design.sv testbench.sv  && unbuffer vvp a.out  
-design.sv:16: warning: @* is sensitive to all 1024 words in array 'memory'.
-Read data (addr 4): abcd1234
-Read data (addr 8): 55555555
-testbench.sv:74: $finish called at 55 (1s)
+Iniciando teste da DataMemory...
+=================================
+[MEM WRITE] Addr: 00000004 : Data: abcd1234
+[MEM READ] Addr: 00000004 : Data: abcd1234
+[MEM WRITE] Addr: 00000ffc : Data: ffffffff
+[MEM READ] Addr: 00000ffc : Data: ffffffff
+[MEM READ ERROR] Endereço 00001000 fora dos limites!
+=================================
+Teste da DataMemory concluído!
