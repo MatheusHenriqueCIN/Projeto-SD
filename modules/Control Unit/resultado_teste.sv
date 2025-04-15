70dfc7beef3cb8497275b94fdb18e717877cb486
Gabriel Machado <158304940+gabriotz@users.noreply.github.com>
2025-04-14 23:41:02 -0300

    Update resultado_teste.sv

--- modules/Control Unit/resultado_teste.sv
+++ modules/Control Unit/resultado_teste.sv
@@ -1,8 +1,10 @@
-
-Tempo                    0: opcode=000000 | RegDst=1, Branch=0, MemRead=0, MemtoReg=0, ALUOp=10, MemWrite=0, ALUSrc=0, RegWrite=1
-Tempo                   30: opcode=100011 | RegDst=0, Branch=0, MemRead=1, MemtoReg=1, ALUOp=00, MemWrite=0, ALUSrc=1, RegWrite=1
-Tempo                   50: opcode=101011 | RegDst=0, Branch=0, MemRead=0, MemtoReg=1, ALUOp=00, MemWrite=1, ALUSrc=1, RegWrite=0
-Tempo                   70: opcode=000100 | RegDst=0, Branch=1, MemRead=0, MemtoReg=1, ALUOp=01, MemWrite=0, ALUSrc=0, RegWrite=0
-Tempo                   90: opcode=001000 | RegDst=0, Branch=0, MemRead=0, MemtoReg=0, ALUOp=00, MemWrite=0, ALUSrc=1, RegWrite=1
-Tempo                  110: opcode=111111 | RegDst=0, Branch=0, MemRead=0, MemtoReg=0, ALUOp=00, MemWrite=0, ALUSrc=0, RegWrite=0
-Testes concluídos!
+Iniciando teste da Control Unit...
+=================================
+Tempo                   10: Opcode=000000 (R-type) | RegDst=1, Branch=0, MemRead=0, MemtoReg=0, ALUOp=10, MemWrite=0, ALUSrc=0, RegWrite=1
+Tempo                   20: Opcode=100011 (lw)     | RegDst=0, Branch=0, MemRead=1, MemtoReg=1, ALUOp=00, MemWrite=0, ALUSrc=1, RegWrite=1
+Tempo                   30: Opcode=101011 (sw)     | RegDst=0, Branch=0, MemRead=0, MemtoReg=1, ALUOp=00, MemWrite=1, ALUSrc=1, RegWrite=0
+Tempo                   40: Opcode=000100 (beq)    | RegDst=0, Branch=1, MemRead=0, MemtoReg=1, ALUOp=01, MemWrite=0, ALUSrc=0, RegWrite=0
+Tempo                   50: Opcode=001000 (addi)   | RegDst=0, Branch=0, MemRead=0, MemtoReg=0, ALUOp=00, MemWrite=0, ALUSrc=1, RegWrite=1
+Tempo                   60: Opcode=111111 (inválido)| RegDst=0, Branch=0, MemRead=0, MemtoReg=0, ALUOp=00, MemWrite=0, ALUSrc=0, RegWrite=0
+=================================
+Teste da Control Unit concluído com sucesso!
