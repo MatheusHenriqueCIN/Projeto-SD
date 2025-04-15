0ca5b48ff8c4ed16cff39a5173ecfefec4440045
Gabriel Machado <158304940+gabriotz@users.noreply.github.com>
2025-04-14 23:55:50 -0300

    Update program_counter.sv

--- modules/PC/program_counter.sv
+++ modules/PC/program_counter.sv
@@ -1,1 +1,17 @@
+module ProgramCounter (
+    input logic clk,           // Clock
+    input logic reset,        // Reset síncrono
+    input logic enable,       // Habilita a atualização do PC
+    input logic [31:0] pc_next, // Próximo valor do PC
+    output logic [31:0] pc     // Valor atual do PC
+);
 
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            pc <= 32'b0;      // Reseta o PC para 0
+        end else if (enable) begin
+            pc <= pc_next;    // Atualiza o PC com o próximo valor
+        end // else: pc mantém o valor anterior (implícito)
+    end
+
+endmodule
